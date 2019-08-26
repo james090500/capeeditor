c884d5cbd3b3e7066b09b9e44b2ca9c4dc9967e2
James Harrison <james@fasttortoise.co.uk>
2019-08-26 01:06:01 +0100

    Drag and draw works template loaded on default Downloads Can change colour

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
     <title>Pixel Editor</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.8/FileSaver.min.js" integrity="sha256-FPJJt8nA+xL4RU6/gsriA8p8xAeLGatoyTjldvQKGdE=" crossorigin="anonymous"></script>
     <style>
       #pixeleditor {
+        border: 1px solid black;
         width: 100%;
         image-rendering: optimizeSpeed;             /* Older versions of FF          */
         image-rendering: -moz-crisp-edges;          /* FF 6.0+                       */
@@ -25,8 +27,10 @@
           <h1>Pixel Editor</h1>
           <canvas id="pixeleditor"></canvas>
           <div class="text-center">
-            <input type="file" id="uploadImg" class="btn">
+            <button type="button" id="loadTemplate" class="btn btn-danger">Load Template</button>
+            <input type="file" id="uploadImg">
             <button type="button" id="downloadImg" class="btn btn-success">Download</button>
+            <input type="color" id="chooseColor"/>
           </div>
         </div>
       </div>
--- pixeleditor.js
+++ pixeleditor.js
@@ -1,18 +1,37 @@
 /* eslint no-undef: 0 */
-
 var canvas
 var context
 var uploadedImage
+var pixelColor = [ 0, 0, 0, 255 ]
+
+var mouseDown = false
 
 function init () {
   listeners()
   canvas = document.getElementById('pixeleditor')
   context = canvas.getContext('2d')
+  loadTemplate(true)
 }
 
 function listeners () {
-  $('#uploadImg').on('change', function (e) { upload(e) })
-  $('#pixeleditor').on('mousedown', function (e) { handleMouseDown(e) })
+  $('#loadTemplate').on('click', () => { loadTemplate(false) })
+  $('#uploadImg').on('change', upload)
+  $('#chooseColor').on('change', updateColor)
+  $('#downloadImg').on('click', download)
+
+  $('#pixeleditor').on('mousedown', function () {
+    mouseDown = true
+  })
+
+  $('#pixeleditor').on('mouseup', function () {
+    mouseDown = false
+  })
+
+  $('#pixeleditor').on('mousemove', function (e) {
+    if (mouseDown) {
+      editPixel(e)
+    }
+  })
 }
 
 function upload (e) {
@@ -28,24 +47,60 @@ function upload (e) {
     uploadedImage.src = event.target.result
   }
   reader.readAsDataURL(e.target.files[0])
-  console.log(e.target.files[0])
 }
 
-function handleMouseDown (e) {
+function editPixel (e) {
   let canvasOffset = $('#pixeleditor').offset()
-  let offsetX = canvasOffset.left
-  let offsetY = canvasOffset.top
-  let pixelOffsetX = 930 / 64
-  let pixelOffsetY = 465 / 32
+  let offsetX = canvasOffset.left - window.pageXOffset
+  let offsetY = canvasOffset.top - window.pageYOffset
+  let pixelOffsetX = $('#pixeleditor').width() / canvas.width
+  let pixelOffsetY = $('#pixeleditor').height() / canvas.height
   let mouseX = parseInt((e.clientX - offsetX) / pixelOffsetX)
   let mouseY = parseInt((e.clientY - offsetY) / pixelOffsetY)
 
   let pxData = context.getImageData(mouseX, mouseY, 1, 1)
-  pxData.data[0] = 255
-  pxData.data[1] = 0
-  pxData.data[2] = 0
+  for (let i = 0; i < 3; i++) {
+    pxData.data[i] = pixelColor[i]
+  }
   pxData.data[3] = 255
+
   context.putImageData(pxData, mouseX, mouseY)
+}
+
+function updateColor () {
+  let hexColor = $('#chooseColor').val()
+  hexColor = hexColor.replace('#', '')
+  pixelColor = []
+  pixelColor[0] = parseInt(hexColor.substring(0, 2), 16)
+  pixelColor[1] = parseInt(hexColor.substring(2, 4), 16)
+  pixelColor[2] = parseInt(hexColor.substring(4, 6), 16)
+  pixelColor[3] = 255
+}
+
+function download () {
+  let unix = parseInt(new Date().getTime() / 1000)
+  canvas.toBlob(function (blob) {
+    saveAs(blob, 'custom-cape-' + unix + '.png')
+  }, 'image/png')
+}
+
+function loadTemplate (autoLoad) {
+  if (!autoLoad) {
+    console.log('test')
+    let confirmed = confirm('Are you sure you want to load the template? It will delete your current cape')
+    if (!confirmed) {
+      return
+    }
+  }
+
+  let template = new Image()
+  template.onload = function () {
+    canvas.width = template.width
+    canvas.height = template.height
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    context.drawImage(template, 0, 0)
+  }
+  template.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAAAgCAYAAACinX6EAAAA/klEQVRoQ+2X0QrDIAxFk08ZjLLv39fsobBPyVDWlYHFq0Rsyu1LX6Imx5urqqTPxPK/+oFhqlqdKi+7r6t2Kw/RNzQXsl4pRnPxihVmpiJgOmjkBgGN7y30aNx0AL/EFsu7YM8yYX2g6NsQhQGQyhoB4TwAUoWL2ZECtn31hkAAs03wr2OpgHoLeHtBuBYgAOfTgAoYZYIiq6jcwXvjboX2wq7lXsfhMAUQABXAFqAH9JhgfhECRuhngm2vRzjaZDUC6DgGqYCvxmptcOkWQFRweQA1CKcHALvl5MDmu/rkfN2XJwB3pMEmpAKCbZh7ulSAO9JgE1IBwTbMPd0PBl+zIeA9OcoAAAAASUVORK5CYII='
 }
 
 init()
